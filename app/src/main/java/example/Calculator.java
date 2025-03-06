17ed51ab50e6d53bdfdee12eae1b360e84d8c6ec
jerry <zhexus@student.unimelb.edu.au>
2025-03-06 11:02:54 +1100

    fix divide 0 situation

--- app/src/main/java/example/Calculator.java
+++ app/src/main/java/example/Calculator.java
@@ -2,7 +2,7 @@ package example;
 
 public class Calculator {
     public int add(int a, int b) {
-        return a+b;
+        return a + b;
     }
 
     public int minus(int a, int b) {
@@ -14,6 +14,10 @@ public class Calculator {
     }
 
     public double divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("can not divide 0");
+        }
         return (double) a / b;
     }
+
 }
